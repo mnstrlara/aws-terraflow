92ee287b47ed4a293fc27ac7a2af4106526aba53
lara <lara.manestar@appon.net>
2024-06-19 18:28:14 +0200

    refactor: iam role rename

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -1,9 +1,9 @@
 output "role_name" {
-  value       = var.create_new_role ? aws_iam_role.codepipeline_role[0].name : data.aws_iam_role.existing_codepipeline_role[0].name
+  value       = var.create_new_role ? aws_iam_role.terraflow-role[0].name : data.aws_iam_role.existing-terraflow-role[0].name
   description = "The IAM Role name"
 }
 
 output "role_arn" {
-  value       = var.create_new_role ? aws_iam_role.codepipeline_role[0].arn : data.aws_iam_role.existing_codepipeline_role[0].arn
+  value       = var.create_new_role ? aws_iam_role.terraflow-role[0].arn : data.aws_iam_role.existing-terraflow-role[0].arn
   description = "The ARN of the IAM Role"
 }
