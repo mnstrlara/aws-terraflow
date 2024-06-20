a03a33d374b155bce935e6da7dcb467db64e030a
lara <lara.manestar@appon.net>
2024-06-20 13:59:04 +0200

    refactor: repo name, default branch

--- terraform/codecommit/variables.tf
+++ terraform/codecommit/variables.tf
@@ -1,12 +1,17 @@
+variable "create_new_repo" {
+  type        = bool
+  description = "Flag for deciding if a new repository needs to be created"
+  default     = false
+}
+
+variable "repo_default_branch" {
+  description = "The name of the default repository branch (default: master)"
+  default     = "master"
+}
+
 variable "source_repository_name" {
   type        = string
   description = "Name of the Source CodeCommit repository used by the pipeline"
-  sensitive   = true
-}
-
-variable "source_repository_branch" {
-  type        = string
-  description = "Branch of the Source CodeCommit repository used in pipeline"
   sensitive   = true
 }
 
